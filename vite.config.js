4cf22d85291ab962b97b0cd330bf3f0abe127a87
lorabucko <lorabucko@gmail.com>
2026-04-22 02:03:41 +0200

    vite

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
+    base: '/Coffe-Joe-Template/',
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
